29c3fc456338c17fc088d9d5fb102e2316884dd1
NNNH <77568569+yhgzs-111@users.noreply.github.com>
2025-05-10 23:54:02 +0800

    Update launch.py

--- src/launch.py
+++ src/launch.py
@@ -3,11 +3,9 @@ from ncatbot.utils import config
 from ncatbot.utils import get_log
 
 _log = get_log()
-config.set_bot_uin("123456")  # 设置 bot qq 号 (必填)
-config.set_ws_uri("ws://localhost:3001")  # 设置 napcat websocket server 地址
 
 bot = BotClient()
 
 if __name__ == "__main__":
     _log.info("Bot 启动中")
-    bot.run()
+    bot.run(bt_uin="123456")
